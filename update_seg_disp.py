a23d2fd686f3e10a815c33df95766791509e3d3e
KYDronePilot <33381603+kydronepilot@users.noreply.github.com>
2018-07-11 20:27:51 -0400

    Had to shorten the delay to keep the LCD screen from blinking in double digit mode.

--- update_seg_disp.py
+++ update_seg_disp.py
@@ -32,8 +32,8 @@ class SegmentController(threading.Thread):
     # Actual thread that is run.
     def run(self):
         while not self.shut.is_set():
-            # Wait a quarter second before checking the queue. Decreases CPU usage by 80%.
-            sleep(0.25)
+            # Wait a little bit before checking the queue.
+            sleep(0.01)
             # If there is something in the queue, update displays.
             if not self.q.empty():
                 # Get dictionary of updated values.
